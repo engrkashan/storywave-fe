d85a32403cf4b1aeb962647811859b9d01a67a87
Muh Qasim <muhammadqasimbhatti4@gmail.com>
2025-12-19 21:15:56 +0500

    :fire:UI

--- src/containers/dashboard/overview/index.jsx
+++ src/containers/dashboard/overview/index.jsx
@@ -159,6 +159,12 @@ const Overview = () => {
                               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
                             </svg>
                           </div>
+                        ) : story.status === "COMPLETED" ? (
+                          <div className="text-green-500">
+                            <svg className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                            </svg>
+                          </div>
                         ) : (
                           <div className="text-blue-500 animate-spin">
                             <svg className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
